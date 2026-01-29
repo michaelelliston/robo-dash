147ede0c7cc540979d4739fc0acf5eda7c8c1f34
Michael <michael.elliston95@gmail.com>
2026-01-29 10:54:12 -0600

    Initial commit

---
+++ src/main/java/com/maxxbyte/robo_dash/RoboDashApplication.java
@@ -0,0 +1,13 @@
+package com.maxxbyte.robo_dash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboDashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoboDashApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxxbyte/robo_dash/RoboDashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxxbyte.robo_dash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoboDashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
