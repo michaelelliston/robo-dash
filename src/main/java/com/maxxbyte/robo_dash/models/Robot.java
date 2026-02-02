bae9497691b4905bc200c021f318893fe8548cab
Michael <michael.elliston95@gmail.com>
2026-02-02 12:00:30 -0600

    Modified Robot model class to implement an enum for status, as well as laying out future method and field plans.

--- src/main/java/com/maxxbyte/robo_dash/models/Robot.java
+++ src/main/java/com/maxxbyte/robo_dash/models/Robot.java
@@ -3,15 +3,37 @@ package com.maxxbyte.robo_dash.models;
 public class Robot {
 
     //TODO: implement battery drain based on distance measurement
-    int battery = 100;
+    int id;
+    int batteryLevel = 100; // Percentage of battery
     int walkwaySpeed = 15;
-    int roadwaySpeed = 30;
-    RobotStatus isAvailable = RobotStatus.IDLE;
+    int roadwaySpeed = 30; //TODO: Consider using an enum for SurfaceType, and assign different speeds to those.
+    RobotStatus status = RobotStatus.IDLE;
     boolean turnSignal = false;
-    int currentOrderId;
+    Order currentOrder;
+    // Location currentLocation;  TODO: Create a Location model
+    // Route currentRoute;   TODO: Create a Route model
 
     public Robot() {
 
     }
 
+    //TODO: implement ability to obtain customer location via their ID, map a route, and then start the delivery.
+    public void startDelivery(Order order, int customerId) {
+        this.currentOrder = order;
+
+    }
+
+
+
+    public int getId() {
+        return this.id;
+    }
+
+    public RobotStatus getStatus() {
+        return status;
+    }
+
+    //public Location getLocation() {}
+
+    //public Route getRoute() {}
 }
