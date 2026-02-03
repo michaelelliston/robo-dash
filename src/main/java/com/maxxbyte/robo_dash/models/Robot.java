b244cb91427e6ee25ed47ab1bc345ffbbc8c854d
Michael <michael.elliston95@gmail.com>
2026-02-03 12:11:21 -0600

    Implementing methods into the Robot model class and adding access modifiers to fields

--- src/main/java/com/maxxbyte/robo_dash/models/Robot.java
+++ src/main/java/com/maxxbyte/robo_dash/models/Robot.java
@@ -3,13 +3,13 @@ package com.maxxbyte.robo_dash.models;
 public class Robot {
 
     //TODO: implement battery drain based on distance measurement
-    int id;
-    int batteryLevel = 100; // Percentage of battery
-    int walkwaySpeed = 15;
-    int roadwaySpeed = 30; //TODO: Consider using an enum for SurfaceType, and assign different speeds to those.
-    RobotStatus status = RobotStatus.IDLE;
-    boolean turnSignal = false;
-    Order currentOrder;
+    private int id;
+    private int batteryLevel = 100; // Percentage of battery
+    private final int walkwaySpeed = 15;
+    private final int roadwaySpeed = 30; //TODO: Consider using an enum for SurfaceType, and assign different speeds to those.
+    private RobotStatus status = RobotStatus.IDLE;
+    private boolean turnSignal = false;
+    private Order currentOrder;
     // Location currentLocation;  TODO: Create a Location model
     // Route currentRoute;   TODO: Create a Route model
 
@@ -18,12 +18,27 @@ public class Robot {
     }
 
     //TODO: implement ability to obtain customer location via their ID, map a route, and then start the delivery.
-    public void startDelivery(Order order, int customerId) {
+    public void assignOrder(Order order, int customerId) {
         this.currentOrder = order;
 
+        if (this.batteryLevel <= 40) {
+            chargeBattery();
+        }
     }
 
+    public void chargeBattery() {
+        if (batteryLevel < 100) {
+            status = RobotStatus.CHARGING;
+            while (batteryLevel < 100) {
+                batteryLevel++;
+            }
+        }
+        status = RobotStatus.IDLE;
+    }
 
+    public void toggleTurnSignal() {
+        turnSignal = !turnSignal;
+    }
 
     public int getId() {
         return this.id;
@@ -33,7 +48,10 @@ public class Robot {
         return status;
     }
 
-    //public Location getLocation() {}
-
-    //public Route getRoute() {}
+    // Parameters are assumed, use as necessary. TODO:
+    // public Location getCustomerLocation(int customerId) {}
+    // public Location getCurrentLocation() {}
+    // public Route mapRoute(Location customerLocation, Location currentLocation) {}
+    // public Route getCurrentRoute() {}
+    //
 }
