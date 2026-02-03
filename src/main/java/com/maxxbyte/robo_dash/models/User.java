c4f382cddce0fa150c8243719dc6ee6fdbb74825
Kigen Jones <kigenjonesrm@gmail.com>
2026-02-03 09:39:19 -0600

    updated user class

--- src/main/java/com/maxxbyte/robo_dash/models/User.java
+++ src/main/java/com/maxxbyte/robo_dash/models/User.java
@@ -1,4 +1,36 @@
 package com.maxxbyte.robo_dash.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 public class User {
+
+    private String username;
+    @JsonIgnore
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{username='" + username + "'}";
+    }
 }
