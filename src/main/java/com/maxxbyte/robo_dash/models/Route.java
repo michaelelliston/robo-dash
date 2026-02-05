86bfa797ba6288e7048ca32aae9300eb6d386ea1
Michael <michael.elliston95@gmail.com>
2026-02-05 13:58:42 -0600

    Created model class for Route and outlined the required fields

---
+++ src/main/java/com/maxxbyte/robo_dash/models/Route.java
@@ -0,0 +1,11 @@
+package com.maxxbyte.robo_dash.models;
+
+import java.util.List;
+
+public class Route {
+
+    private Location startingLocation;
+    private Location destination;
+    private List<Path> paths;
+    private int totalDistance;
+}
