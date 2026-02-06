704375c48b81d72cdf4a5f1170c80d9438b86a79
Michael <michael.elliston95@gmail.com>
2026-02-06 12:28:36 -0600

    Added getter and setter methods

--- src/main/java/com/maxxbyte/robo_dash/models/Location.java
+++ src/main/java/com/maxxbyte/robo_dash/models/Location.java
@@ -7,4 +7,44 @@ public class Location {
     LocationType type;
     long latitude;
     long longitude;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocationType getType() {
+        return type;
+    }
+
+    public void setType(LocationType type) {
+        this.type = type;
+    }
+
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(long latitude) {
+        this.latitude = latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(long longitude) {
+        this.longitude = longitude;
+    }
 }
--- src/main/java/com/maxxbyte/robo_dash/models/Path.java
+++ src/main/java/com/maxxbyte/robo_dash/models/Path.java
@@ -7,4 +7,44 @@ public class Path {
     int toLocationId;
     int distance; // measured in meters
     PathType pathType;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getFromLocationId() {
+        return fromLocationId;
+    }
+
+    public void setFromLocationId(int fromLocationId) {
+        this.fromLocationId = fromLocationId;
+    }
+
+    public int getToLocationId() {
+        return toLocationId;
+    }
+
+    public void setToLocationId(int toLocationId) {
+        this.toLocationId = toLocationId;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public PathType getPathType() {
+        return pathType;
+    }
+
+    public void setPathType(PathType pathType) {
+        this.pathType = pathType;
+    }
 }
