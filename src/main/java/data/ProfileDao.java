ddbe4861d464f135e66a47d42d2a99dae31ff557
humza <humzaqasim1@gmail.com>
2026-02-10 11:18:54 -0600

    Created ProfileDao

---
+++ src/main/java/data/ProfileDao.java
@@ -0,0 +1,29 @@
+package data;
+
+import com.maxxbyte.robo_dash.models.Product;
+import com.maxxbyte.robo_dash.models.Profile;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Component
+public class ProfileDao extends DaoBase{
+    public ProfileDao(DataSource dataSource) {super(dataSource);}
+
+    private Profile mapRow(ResultSet row) throws SQLException
+    {
+        int userId = row.getInt("user_id");
+        String firstName = row.getString("first_name");
+        String lastName = row.getString("last_name");
+        String phone = row.getString("phone");
+        String email = row.getString("email");
+        String address = row.getString("address");
+        String city = row.getString("city");
+        String state = row.getString("state");
+        String zip = row.getString("zip");
+
+        return new Profile(userId, firstName, lastName, phone, email, address, city, state, zip);
+    }
+}
