46f4cb9b4dad5c5d49a5cd35f9f6b947168e3b82
humza <humzaqasim1@gmail.com>
2026-02-10 11:07:32 -0600

    Created Maprow method in LocationDao

--- src/main/java/data/LocationDao.java
+++ src/main/java/data/LocationDao.java
@@ -22,18 +22,15 @@ public class LocationDao extends DaoBase{
 //        //return;
 //    }
 
-    private Location mapRow(ResultSet row) throws SQLException
-    {
-        int id = row.getInt("location_id");
-        String name = row.getString("building_name");
-//        LocationType locationType = row.getString("description");
-
-        Location location = new Location()
-        {{
-        //    setid(location_id);
-
-        }};
-
-        return location;
-    }
+//    private Location mapRow(ResultSet row) throws SQLException
+//    {
+//        int id = row.getInt("location_id");
+//        String name = row.getString("building_name");
+//        String locationString = row.getString("location_type");
+//        LocationType locationType = LocationType.valueOf(locationString);
+//        long latitude = row.getLong("latitude");
+//        long longitude = row.getLong("longitude");
+//
+//        return new Location(id,name,locationType,latitude,longitude);
+//    }
 }
