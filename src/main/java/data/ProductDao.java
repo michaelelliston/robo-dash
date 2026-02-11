1102c08a8ff356a76543a9bb21b890fdd79217f4
humza <humzaqasim1@gmail.com>
2026-02-11 10:39:58 -0600

    updated productdao

--- src/main/java/data/ProductDao.java
+++ src/main/java/data/ProductDao.java
@@ -5,16 +5,37 @@ import com.maxxbyte.robo_dash.models.Product;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 @Component
 public class ProductDao extends DaoBase {
     public ProductDao(DataSource dataSource) {super(dataSource);}
 
+    public Product getById(int productId)
+    {
+        String sql = "SELECT * FROM products WHERE product_id = ?";
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, productId);
 
+            ResultSet row = statement.executeQuery();
 
-
+            if (row.next())
+            {
+                return mapRow(row);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
 
     private Product mapRow(ResultSet row) throws SQLException
     {
