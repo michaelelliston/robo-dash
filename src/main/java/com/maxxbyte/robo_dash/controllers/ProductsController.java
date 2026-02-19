79ae3acb5f92df36af39183b6bc5e4656a6e088e
Michael <michael.elliston95@gmail.com>
2026-02-19 11:09:22 -0600

    Updated ProductsController with @PreAuthorize annotation on getById() method

--- src/main/java/com/maxxbyte/robo_dash/controllers/ProductsController.java
+++ src/main/java/com/maxxbyte/robo_dash/controllers/ProductsController.java
@@ -4,10 +4,10 @@ import com.maxxbyte.robo_dash.data.ProductDao;
 import com.maxxbyte.robo_dash.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
-import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -22,7 +22,7 @@ public class ProductsController {
     }
 
     @GetMapping("{id}")
-    //@PreAuthorize("permitAll()")
+    @PreAuthorize("permitAll()")
     public Product getById(@PathVariable int id) {
         try {
             Product product = productDao.getById(id);
@@ -30,6 +30,7 @@ public class ProductsController {
             if (Objects.isNull(product)) {
                 throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
             }
+
             return product;
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Server error");
