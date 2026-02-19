cb9bff4bc00d041a4cfb54c47e910cd6eb09c494
Michael <michael.elliston95@gmail.com>
2026-02-19 10:57:41 -0600

    Created initial ProductsController class

---
+++ src/main/java/com/maxxbyte/robo_dash/controllers/ProductsController.java
@@ -0,0 +1,38 @@
+package com.maxxbyte.robo_dash.controllers;
+
+import com.maxxbyte.robo_dash.data.ProductDao;
+import com.maxxbyte.robo_dash.models.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("products")
+@CrossOrigin
+public class ProductsController {
+    private ProductDao productDao;
+
+    @Autowired
+    public ProductsController(ProductDao productDao) {
+        this.productDao = productDao;
+    }
+
+    @GetMapping("{id}")
+    //@PreAuthorize("permitAll()")
+    public Product getById(@PathVariable int id) {
+        try {
+            Product product = productDao.getById(id);
+
+            if (Objects.isNull(product)) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+            }
+            return product;
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Server error");
+        }
+    }
+}
