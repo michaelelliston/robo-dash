5fcf4f1bcc8f1f555feed613ccc60841c6b2c864
Michael <michael.elliston95@gmail.com>
2026-02-20 13:08:26 -0600

    Implemented getAllProducts() and getByCategory() methods in ProductsController

--- src/main/java/com/maxxbyte/robo_dash/controllers/ProductsController.java
+++ src/main/java/com/maxxbyte/robo_dash/controllers/ProductsController.java
@@ -4,10 +4,12 @@ import com.maxxbyte.robo_dash.data.ProductDao;
 import com.maxxbyte.robo_dash.models.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
 import java.util.Objects;
 
 @RestController
@@ -21,6 +23,20 @@ public class ProductsController {
         this.productDao = productDao;
     }
 
+    @GetMapping("")
+    @PreAuthorize("permitAll()")
+    public List<Product> getAllProducts() {
+        try {
+            List<Product> products = productDao.getAll();
+            if (products.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
+            return products;
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Server error");
+        }
+    }
+
     @GetMapping("{id}")
     @PreAuthorize("permitAll()")
     public Product getById(@PathVariable int id) {
@@ -32,6 +48,20 @@ public class ProductsController {
             }
 
             return product;
+        } catch (Exception ex) {
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Server error");
+        }
+    }
+
+    @GetMapping("category/{id}")
+    @PreAuthorize("permitAll()")
+    public List<Product> getByCategory(@PathVariable int id) {
+        try {
+            List<Product> products = productDao.listByCategoryId(id);
+            if (products.isEmpty()) {
+                throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            }
+            return products;
         } catch (Exception ex) {
             throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Server error");
         }
